48095aee3b11f56357a9402a936e4be448d0f335
kathiravanbtm <kathiravanb.aiml2023@citchennai.net>
2026-03-10 17:00:39 +0530

    i don't know what i am doing

--- src/app.ts
+++ src/app.ts
@@ -39,7 +39,7 @@ app.use(
       directives: {
         defaultSrc: ["'self'"],
         scriptSrc: ["'self'", "'unsafe-inline'", "'unsafe-eval'"],
-        scriptSrcAttr: ["'unsafe-inline'"], // required for inline onclick/onsubmit handlers
+        scriptSrcAttr: ["'unsafe-inline'"],
         styleSrc: ["'self'", "'unsafe-inline'", "https://fonts.googleapis.com"],
         fontSrc: ["'self'", "https://fonts.gstatic.com"],
         imgSrc: ["'self'", "data:"],
